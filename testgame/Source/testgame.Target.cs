be8a2a7eed83a8eb1a4d7fde82747270bbf4bb42
KevinRusev <105234590+KevinRusev@users.noreply.github.com>
2026-01-28 10:22:00 +0200

    UE5 project setup

---
+++ testgame/Source/testgame.Target.cs
@@ -0,0 +1,14 @@
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class testgameTarget : TargetRules
+{
+	public testgameTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		ExtraModuleNames.Add("testgame");
+	}
+}
---
+++ testgame/Source/testgame/testgame.cpp
@@ -0,0 +1,7 @@
+
+#include "testgame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, testgame, "testgame" );
+
+DEFINE_LOG_CATEGORY(Logtestgame)
