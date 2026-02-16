3d97a759cd28b34e6ead446be039d19fa9a96b6f
KevinRusev <105234590+KevinRusev@users.noreply.github.com>
2026-02-16 20:24:00 +0200

    Combat life bar UI

---
+++ testgame/Source/testgame/Variant_Combat/UI/CombatLifeBar.h
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CombatLifeBar.generated.h"
+
+UCLASS(abstract)
+class UCombatLifeBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Life Bar")
+	void SetLifePercentage(float Percent);
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Life Bar")
+	void SetBarColor(FLinearColor Color);
+};
