711e266c1a85da0791230d55163f559e52cb36ef
KevinRusev <105234590+KevinRusev@users.noreply.github.com>
2026-03-01 17:12:00 +0200

    Side-scroller UI

---
+++ testgame/Source/testgame/Variant_SideScrolling/UI/SideScrollingUI.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SideScrollingUI.generated.h"
+
+UCLASS(abstract)
+class USideScrollingUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintImplementableEvent, Category="UI")
+	void UpdatePickups(int32 Amount);
+};
