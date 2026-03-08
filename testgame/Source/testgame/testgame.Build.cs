396098a854d6635c44faea35d0f6ba967d9ee43e
KevinRusev <105234590+KevinRusev@users.noreply.github.com>
2026-03-08 21:18:00 +0200

    Update build deps

--- testgame/Source/testgame/testgame.Build.cs
+++ testgame/Source/testgame/testgame.Build.cs
@@ -1,4 +1,3 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -27,6 +26,7 @@ public class testgame : ModuleRules
 
 		PublicIncludePaths.AddRange(new string[] {
 			"testgame",
+			"testgame/Combat",
 			"testgame/Inventory",
 			"testgame/Enemy",
 			"testgame/Variant_Platforming",
@@ -44,12 +44,5 @@ public class testgame : ModuleRules
 			"testgame/Variant_SideScrolling/UI"
 		});
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
